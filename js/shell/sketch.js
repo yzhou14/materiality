008149f4a2a72a4061667faf79f225552653188d
Yue Zhou <yzhou14@risd.edu>
2025-05-20 08:30:46 -0400

    almost

--- js/shell/sketch.js
+++ js/shell/sketch.js
@@ -1,20 +1,6 @@
-/**
- *
- * Ocean系列-海报1
- * Coral Decay
- * 
- * 主程序
- * 
- * 交互内容：
- * 1、鼠标点击改变珊瑚颜色
- * 鼠标松开恢复（连续点击，产生变异效果）
- *
- */
-
-
-var size = 200; //珊瑚大小，在以(centerX,centerY)为圆心，size为半径的区域生成珊瑚
-var corals = []; //存放珊瑚小球
-var isPressed = 0; //鼠标是否按住
+var size = 200; //coral size
+var corals = []; //coral particle storage
+var isPressed = 0; //if mouse is presses
 
 function setup() {
     // 获取容器宽高
@@ -32,29 +18,28 @@ function setup() {
 function draw() {
     background(0, 5);
 
-    //当鼠标按住时
+    //whe pressed
     if (mouseIsPressed == true) {
         isPressed = 1;
     }
 
-    //珊瑚
+    //coral
     corals.push(new Coral(width / 2, height / 2 + 105, width / 2, height / 2 - 25));
     for (let i = 0; i < corals.length; i++) {
-        if (corals[i].size < 0) { //即时删除r<0的小球
+        if (corals[i].size < 0) { //delete r<0 particle
             corals.splice(i, 1);
         }
         corals[i].drawCoral();
         corals[i].move();
-        //print(corals.length); //观察到数组的长度动态平衡在200左右
     }
 }
 
 function init() {
     background(0, 5);
-    frameRate(30); //指定帧率，每秒30帧（30次draw()）
+    frameRate(30);
 }
 
-//鼠标松开，恢复原状
+//mouse release
 function mouseReleased() {
     isPressed = 0;
 }
